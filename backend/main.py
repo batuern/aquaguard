c9d68c5c0271736bd21dbb23e5aa9a5b64af5025
batuern <149186542+batuern@users.noreply.github.com>
2026-02-05 21:51:03 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -9,9 +9,8 @@ import pandas as pd
 
 app = FastAPI(title="AquaGuard AI Backend (MVP)")
 
-REPO_ROOT = Path(__file__).resolve().parents[1]
-FRONTEND_DIR = REPO_ROOT / "proj"
 
+FRONTEND_DIR = Path(__file__).resolve().parents[1] / "proj"
 if FRONTEND_DIR.exists():
     # root "/" üzerinden proj/index.html servisi
     app.mount("/", StaticFiles(directory=str(FRONTEND_DIR), html=True), name="frontend")
